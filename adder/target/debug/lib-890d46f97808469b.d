b5ed4271827bef5fb064e5175f684dd92573aa63
CHEN Mingyi <imcmy@outlook.com>
2015-11-30 15:05:37 +0800

    Add Adder
    Add Adder

---
+++ adder/target/debug/lib-890d46f97808469b.d
@@ -0,0 +1,4 @@
+/Users/imcmy/Documents/05 Workspace/Rust/test/target/debug/lib-890d46f97808469b: tests/lib.rs
+
+/Users/imcmy/Documents/05 Workspace/Rust/test/target/debug/lib-890d46f97808469b.d: tests/lib.rs
+
